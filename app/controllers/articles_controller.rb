89a4187422a13cf0bad40300ecb1584027547ebe
Umair Arshad <Umair6622@gmail.com>
2020-12-12 13:33:02 +0500

    minor changes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,7 +19,7 @@ class ArticlesController < ApplicationController
     @article = current_user.articles.build(article_params)
     if @article.save
       redirect_to article_path(@article)
-    else 
+    else
       render 'new'
     end
   end

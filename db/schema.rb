2e3f787f8af1d9786f1e037d0a04ee9d63a4dbad
Umair Arshad <Umair6622@gmail.com>
2020-12-12 23:31:50 +0500

    Update the voter model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_11_225310) do
+ActiveRecord::Schema.define(version: 2020_12_12_135010) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,12 +75,18 @@ ActiveRecord::Schema.define(version: 2020_12_11_225310) do
   end
 
   create_table "votes", force: :cascade do |t|
+    t.bigint "article_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["article_id"], name: "index_votes_on_article_id"
+    t.index ["user_id"], name: "index_votes_on_user_id"
   end
 
   add_foreign_key "comments", "articles"
   add_foreign_key "posts", "users"
   add_foreign_key "taggings", "articles"
   add_foreign_key "taggings", "tags"
+  add_foreign_key "votes", "articles"
+  add_foreign_key "votes", "users"
 end

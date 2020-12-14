eaea0e31ed43b3922b4346750304e2766acdcc25
Umair Arshad <Umair6622@gmail.com>
2020-12-15 02:27:02 +0500

    Add the test cases

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -2,9 +2,30 @@ require 'rails_helper'
 
 RSpec.describe Article, type: :model do
   describe Article do
+    let(:user_umair) { User.create(email: 'umair6622@gmail.com', password: 'password', password_confirmation: 'password') }
+    let(:subject) do
+      described_class.new(
+        title: 'Resume',
+        body: 'Resume article body',
+        user_id: user_umair.id,
+        image: nil
+      )
+    end
+
+    context 'validations' do
+
+      it 'Content cannot be less than 10' do
+        subject.body = 'text'
+        expect(subject).not_to be_valid
+      end
+    end
     context 'validates title' do
       it '' do
       end
     end
+    
+    
+  
+    
   end
 end
